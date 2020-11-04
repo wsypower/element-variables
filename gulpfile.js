0247691e96bdeee4f5abafaa6729386233dab223
weiyafei <2553241022@qq.com>
2020-11-04 20:24:46 +0800

    📦 NEW: element暗色系列背景色切换

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var path = require('path')
-var gulp = require('gulp')
-var cleanCSS = require('gulp-clean-css');
-var cssWrap = require('gulp-css-wrap');
+var path = require("path");
+var gulp = require("gulp");
+var cleanCSS = require("gulp-clean-css");
+var cssWrap = require("gulp-css-wrap");
 
-var customThemeName='.custom-theme'
+var customThemeName = ".theme-element";
 
-gulp.task('css-wrap', function() {
-  return gulp.src( path.resolve('./theme/index.css'))
-    .pipe(cssWrap({selector:customThemeName}))
+gulp.task("css-wrap", function () {
+  return gulp
+    .src(path.resolve("./theme/index.css"))
+    .pipe(cssWrap({ selector: customThemeName }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("../d2-admin-start-kit/src/assets/style/element-variables"));
 });
 
-gulp.task('move-font', function() {
-  return gulp.src(['./theme/fonts/**']).pipe(gulp.dest('dist/fonts'));
+gulp.task("move-font", function () {
+  return gulp.src(["./theme/fonts/**"]).pipe(gulp.dest("dist/fonts"));
 });
 
-gulp.task('default',['css-wrap','move-font']);
+gulp.task("default", ["css-wrap", "move-font"]);
